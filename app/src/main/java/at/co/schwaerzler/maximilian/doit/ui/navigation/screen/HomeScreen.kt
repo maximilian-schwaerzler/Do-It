d1e1f9764c57e87a9c6ae5353122ce366590eede
Maximilian Schwärzler <maschwaerzler@a1.net>
2026-04-30 22:30:14 +0200

    Add a top app bar.

--- app/src/main/java/at/co/schwaerzler/maximilian/doit/ui/navigation/screen/HomeScreen.kt
+++ app/src/main/java/at/co/schwaerzler/maximilian/doit/ui/navigation/screen/HomeScreen.kt
@@ -3,14 +3,30 @@ package at.co.schwaerzler.maximilian.doit.ui.navigation.screen
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import at.co.schwaerzler.maximilian.doit.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier) {
-    Scaffold(modifier.fillMaxSize()) { innerPadding ->
+    Scaffold(modifier.fillMaxSize(), topBar = {
+        TopAppBar(
+            title = {
+                Text(stringResource(R.string.app_name))
+            },
+            colors = TopAppBarDefaults.topAppBarColors(
+                containerColor = MaterialTheme.colorScheme.surfaceContainer
+            )
+        )
+    }) { innerPadding ->
         Column(Modifier.padding(innerPadding)) {
             Text("Home Screen")
         }
