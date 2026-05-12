0e90b29035243c7f795e53da4b6e32075e2273e8
Maximilian Schwärzler <maschwaerzler@a1.net>
2026-05-12 21:02:51 +0200

    Update SettingsScreen with basic Scaffold layout
    Adds a `Scaffold` and `Column` to `SettingsScreen` to establish the base layout structure and handle content padding.

--- app/src/main/java/at/co/schwaerzler/maximilian/doit/ui/navigation/screen/SettingsScreen.kt
+++ app/src/main/java/at/co/schwaerzler/maximilian/doit/ui/navigation/screen/SettingsScreen.kt
@@ -16,10 +16,20 @@
 
 package at.co.schwaerzler.maximilian.doit.ui.navigation.screen
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 @Composable
 fun SettingsScreen(modifier: Modifier = Modifier) {
-    
+    Scaffold(modifier.fillMaxSize()) { innerPadding ->
+        Column(Modifier
+            .padding(innerPadding)
+            .fillMaxSize()) {
+
+        }
+    }
 }
